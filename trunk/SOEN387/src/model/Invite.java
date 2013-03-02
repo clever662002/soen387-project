6476bdff3d7e7e02c5b3a0e40f58cc3f1b7a31ea
alexmichaelperkins@gmail.com <alexmichaelperkins@gmail.com@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-03-02 22:32:10 +0000

    updates

--- trunk/SOEN387/src/model/Invite.java
+++ trunk/SOEN387/src/model/Invite.java
@@ -2,11 +2,34 @@ package model;
 
 /**
  * Domain model representing an Invite
- * @author Alex
+ * @author Alex Perkins
  */
 public class Invite {
 
 	private int id;
 	private long version;
-	private int groupId;
+	private Group group;
+	
+	public Invite(int id, Group group, long version){
+		this.id = id;
+		this.version = version;
+		this.group = group;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public long getVersion() {
+		return version;
+	}
+	
+	public Group getGroup() {
+		return group;
+	}
+	
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+	
 }
--- trunk/SOEN387/src/model/User.java
+++ trunk/SOEN387/src/model/User.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Domain model representing a user
  * @author Alex
@@ -12,6 +15,7 @@ public class User {
 	private String username;
 	private String password; //Should we keep the password in the object??
 	private int version;
+	private List<Invite> invites;
 	
 	public User(long id, String firstName, String lastName, String username, int version){
 		this.id=id;
@@ -19,10 +23,67 @@ public class User {
 		this.lastName=lastName;
 		this.username=username;
 		this.version=version;
+		this.invites = new ArrayList<Invite>();
+	}
+	
+	public User(String username, String firstName, String lastName, String password){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.invites = new ArrayList<Invite>();
 	}
 	
 	public String toString(){
 		return "User ID: "+id+ ", username: " + username+ ", first name: " +firstName+ ", last name: " + lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+	
+	public void setInvites(List<Invite> invites){
+		this.invites = invites;
+	}
+	
+	public List<Invite> getInvites(){
+		return invites;
 	}
 	
 }
