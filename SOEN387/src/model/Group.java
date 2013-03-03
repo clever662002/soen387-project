f90980a7fbf9a1188371d360cb0c1f057a24ba3b
tungque88 <tungque88@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-03-03 02:18:15 +0000

    Group update

--- SOEN387/src/model/Group.java
+++ SOEN387/src/model/Group.java
@@ -11,7 +11,42 @@ public class Group {
 	private int id;
 	private String name;
 	private String description;
-	private List<User> users;
-	private long version;
+	//private List<User> users;
+	private int version;
 	
+	
+	public Group(int id, String name, String description, int version) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.version = version;
+	}
+		
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
 }
