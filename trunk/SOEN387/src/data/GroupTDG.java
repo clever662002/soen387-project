264d527ed942b941a56e60f64969dde42971ee0a
tungque88 <tungque88@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-03-03 02:18:52 +0000

    GroupTDG update

--- trunk/SOEN387/src/data/GroupTDG.java
+++ trunk/SOEN387/src/data/GroupTDG.java
@@ -3,7 +3,6 @@ package data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 
@@ -16,24 +15,26 @@ public class GroupTDG {
 	private static final String DB_PREFIX = "groupformation_";
 	private static final String DB_NAME = DB_PREFIX+"group";
 	
-	private static String SELECT = "SELECT * FROM " + DB_NAME + " WHERE username=?";
+	private static String SELECT_BY_ID = "SELECT * FROM " + DB_NAME + " WHERE group_id = ?";
+	private static String SELECT_BY_NAME = "SELECT * FROM " + DB_NAME + " WHERE name = ?";
 	private static String SELECT_ALL = "SELECT * FROM " + DB_NAME;
-	private static String FIND = "SELECT * FROM "+DB_NAME+","+DB_PREFIX+"user_group WHERE groups.group_id=user_group.group_id AND user_group.user_id =?";
+	private static String INSERT = "INSERT INTO " + DB_NAME + " (name,description,version) " +
+			                           " VALUES((select max(group_id)+1 FROM " + DB_NAME + ")," +
+			                           		    " name,description,1)";
+	private static String UPDATE = "UPDATE " + DB_NAME + " SET group_id = ?, name = ?, description = ?, version = ? WHERE group_id = ?";
+	private static String DELETE = "DELETE FROM " + DB_NAME + "WHERE group_id = ?";
 	
-	
-	public static ResultSet find(String username) throws SQLException {
-	/*	
-		User result = null;		
-		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(FIND_BY_USERNAME);
-		ps.setString(1, username);
+	public static ResultSet find(String group_name) throws SQLException 
+	{
+		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(SELECT_BY_NAME);
+		ps.setString(1, group_name);
 		ResultSet rs = ps.executeQuery();
+		ps.close();
 		return rs;
-		*/
-		return null;
 	}
 	
 	public static ResultSet find(int id) throws SQLException {
-		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(SELECT);
+		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(SELECT_BY_ID);
 		ps.setString(1, id+"");
 		ResultSet rs = ps.executeQuery();
 		ps.close();
@@ -48,4 +49,35 @@ public class GroupTDG {
 		return rs;
 	}
 	
+	public static void insert(String name, String description) 
+	throws SQLException
+	{
+		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(INSERT);
+		ps.setString(1, name);
+		ps.setString(2, description);		
+		ps.executeUpdate();
+		ps.close();
+	}
+	
+	public static int update(int group_id, String name, String description, int version) throws SQLException
+	{
+		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(UPDATE);
+		ps.setInt(1, group_id);
+		ps.setString(2, name);
+		ps.setString(3, description);
+		ps.setInt(4, version);
+		int count = ps.executeUpdate();
+		ps.close();
+		return count;
+	}
+	
+	public static int delete(int group_id, String name, String description, int version) throws SQLException
+	{
+		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(DELETE);
+		ps.setInt(1, group_id);
+		ps.setInt(2, version);
+		int count = ps.executeUpdate();
+		ps.close();
+		return count;
+	}
 }
