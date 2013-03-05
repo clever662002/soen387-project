878ce667aed357ffe8f2b8eb8305f94c83bf9f40
tungque88 <tungque88@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-03-05 22:15:57 +0000

    grouptdg

--- SOEN387/src/data/GroupTDG.java
+++ SOEN387/src/data/GroupTDG.java
@@ -3,10 +3,6 @@ package data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Vector;
-
-import model.User;
 
 import org.dsrg.soenea.service.threadLocal.DbRegistry;
 
@@ -21,10 +17,12 @@ public class GroupTDG {
 	private static String SELECT_BY_ID = "SELECT * FROM " + TABLE + " WHERE group_id = ?";
 	private static String SELECT_BY_NAME = "SELECT * FROM " + TABLE + " WHERE name = ?";
 	private static String SELECT_ALL = "SELECT g.group_id, g.name, g.description, g.version FROM " + TABLE + " AS g;";
-	private static String INSERT = "INSERT INTO " + TABLE + " (name,description,version) " + " VALUES((select max(group_id)+1 FROM " + TABLE + ")," +
-			                           		    " name,description,1)";
+	//private static String INSERT = "INSERT INTO " + TABLE + " (name,description,version) " + " VALUES((select max(group_id)+1 FROM " + TABLE + ")," +
+	//		                           		    " name,description,1)";
+	private static String INSERT = "INSERT INTO " + TABLE + " (name,description) " + " VALUES(?,?)";
 	private static String UPDATE = "UPDATE " + TABLE + " SET group_id = ?, name = ?, description = ?, version = ? WHERE group_id = ?";
-	private static String DELETE = "DELETE FROM " + TABLE + "WHERE group_id = ?";
+	//private static String UPDATE = "UPDATE " + TABLE + " SET name = ?, description = ?  WHERE group_id = ?";
+	private static String DELETE = "DELETE FROM " + TABLE + " WHERE group_id = ?";
 	
 	
 	public static ResultSet findAll() throws SQLException {
@@ -52,10 +50,12 @@ public class GroupTDG {
 	}
 
 	
-	public static void insert(String name, String description) 
+	//public static void insert(int group_id, String name, String description) 
+	public static void insert(String name, String description)
 	throws SQLException
 	{
 		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(INSERT);
+		//ps.setInt(1, group_id);
 		ps.setString(1, name);
 		ps.setString(2, description);		
 		ps.executeUpdate();
@@ -69,6 +69,7 @@ public class GroupTDG {
 		ps.setString(2, name);
 		ps.setString(3, description);
 		ps.setInt(4, version);
+		ps.setInt(5, group_id);
 		int count = ps.executeUpdate();
 		ps.close();
 		return count;
@@ -76,9 +77,11 @@ public class GroupTDG {
 	
 	public static int delete(int group_id, String name, String description, int version) throws SQLException
 	{
+		System.out.println("Group id" + group_id);
+		System.out.println(DbRegistry.getDbConnection().prepareStatement(DELETE));
 		PreparedStatement ps = DbRegistry.getDbConnection().prepareStatement(DELETE);
 		ps.setInt(1, group_id);
-		ps.setInt(2, version);
+		//ps.setInt(2, version);
 		int count = ps.executeUpdate();
 		ps.close();
 		return count;
