f5a9cc17537950c2e261df207d05e8496c6b4c9f
tinawang2802 <tinawang2802@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-03-06 03:06:05 +0000

    ViewGroupPC

--- trunk/SOEN387/src/controller/ViewGroupPC.java
+++ trunk/SOEN387/src/controller/ViewGroupPC.java
@@ -11,7 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import mapper.GroupMapper;
+import mapper.UserMapper;
 import model.Group;
+import model.GroupProxy;
+import model.User;
 
 
 public class ViewGroupPC extends BaseHttpServlet {
@@ -19,19 +22,29 @@ public class ViewGroupPC extends BaseHttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private static final String VIEW_NAME = "/jsp/ViewGroupTV.jsp";
+	private static final String NO_GROUP = "/jsp/NoGroupTV.jsp";
+	
 		
 	@Override
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try
 		{
-			String sGroupID = request.getParameter("group_id");
+			int userId = Integer.parseInt(request.getSession().getAttribute("user_id")+"");
+			User user = UserMapper.find(userId);
 			
-			if(sGroupID != null)
-			{
-				request.setAttribute("group", GroupMapper.find(Integer.parseInt(sGroupID)));
+			GroupProxy gp = user.getGroup();
+			int sGroupId = gp.getId();
+			
+			Group gr = GroupMapper.find(sGroupId);			
+			String sGroupID = request.getParameter("group_id:");
+			// if the user doesn't register for a group right now.
+			if(gr == null){
+				request.getRequestDispatcher(NO_GROUP).forward(request, response);
+			}else {
+				request.setAttribute("group", gr);
+				request.getRequestDispatcher(VIEW_NAME).forward(request, response);
 			}
-			
-			request.getRequestDispatcher(VIEW_NAME).forward(request, response);
+
 		}
 		catch(Exception e)
 		{
