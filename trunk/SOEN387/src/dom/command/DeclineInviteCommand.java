8f3ffaf92bfbf86c06d5f42d339814a10edd0d0e
alexmichaelperkins@gmail.com <alexmichaelperkins@gmail.com@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-04-03 03:10:35 +0000

    Adding base commands

---
+++ trunk/SOEN387/src/dom/command/DeclineInviteCommand.java
@@ -0,0 +1,40 @@
+package dom.command;
+
+import org.dsrg.soenea.domain.command.CommandError;
+import org.dsrg.soenea.domain.command.CommandException;
+import org.dsrg.soenea.domain.command.impl.Command;
+import org.dsrg.soenea.domain.helper.Helper;
+
+public class DeclineInviteCommand extends Command {
+
+	public DeclineInviteCommand(Helper helper){
+		super(helper);
+	}
+	
+	@Override
+	public void execute() throws CommandException {
+		// TODO Auto-generated method stub
+		super.execute();
+	}
+
+	@Override
+	public void setUp() throws CommandException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void process() throws CommandException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tearDown() throws CommandError {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
