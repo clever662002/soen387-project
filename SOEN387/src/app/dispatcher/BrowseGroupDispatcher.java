e28611591e3c92ff5d59ed0335d5ccf1b4dba27d
tungque88 <tungque88@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-04-04 04:23:05 +0000

    added BrowseGroupDispatcher.java

---
+++ SOEN387/src/app/dispatcher/BrowseGroupDispatcher.java
@@ -0,0 +1,26 @@
+package app.dispatcher;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.dsrg.soenea.application.servlet.dispatcher.Dispatcher;
+import dom.command.BrowseGroupCommand;
+
+public class BrowseGroupDispatcher extends Dispatcher{	
+	@Override
+	public void execute() throws ServletException, IOException
+	{
+		try
+		{
+			new BrowseGroupCommand(myHelper).execute();
+			forward("/WEB-INF/jsp/BrowseGroupTV.jsp");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			forward("/WEB-INF/jsp/BrowseGroupTV.jsp");
+		}
+	}
+	 
+}
