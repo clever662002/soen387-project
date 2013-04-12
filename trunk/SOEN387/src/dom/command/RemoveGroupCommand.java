82ae1aaffc35bbc31cf812fc8263ee10ba406d06
tinawang2802 <tinawang2802@53973af4-d9f5-982a-cca4-0fdf2bfb9c45>
2013-04-12 23:15:31 +0000

    remove group

---
+++ trunk/SOEN387/src/dom/command/RemoveGroupCommand.java
@@ -0,0 +1,53 @@
+package dom.command;
+
+import java.util.List;
+
+import org.dsrg.soenea.domain.command.CommandError;
+import org.dsrg.soenea.domain.command.CommandException;
+import org.dsrg.soenea.domain.command.impl.Command;
+import org.dsrg.soenea.domain.helper.Helper;
+
+import dom.model.group.Group;
+import dom.model.group.mappers.GroupMapper;
+
+public class RemoveGroupCommand extends Command{
+
+	public RemoveGroupCommand(Helper helper) {
+		super(helper);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setUp() throws CommandException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void process() throws CommandException {
+		try
+		{
+			String sGroupID   = helper.getString("group_id");
+			// delete requested group
+			Group g = GroupMapper.find(Integer.parseInt(sGroupID));
+			String notice = g.getName() + "has been deleted";
+			helper.setRequestAttribute("notice", notice);
+			GroupMapper.delete(g);
+			List<Group> groups = GroupMapper.findAll();
+			helper.setRequestAttribute("groups",groups);
+			helper.setRequestAttribute("template_view","/WEB-INF/jsp/BrowseGroupTV.jsp");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void tearDown() throws CommandError {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
